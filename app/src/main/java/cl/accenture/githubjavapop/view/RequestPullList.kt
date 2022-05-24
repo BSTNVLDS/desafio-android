47c0b458d2995bccdce2f1443279dd0380eedcc1
BSTNVLDS <bastianvaldesadasme@gmail.com>
2022-05-24 12:32:57 -0400

    load all pulls(while)

--- app/src/main/java/cl/accenture/githubjavapop/view/RequestPullList.kt
+++ app/src/main/java/cl/accenture/githubjavapop/view/RequestPullList.kt
@@ -2,6 +2,7 @@ package cl.accenture.githubjavapop.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +18,7 @@ import kotlinx.coroutines.launch
 class RequestPullList : AppCompatActivity() {
     private lateinit var binding :ActivityRequestpulllistBinding
     private val adapter = PullAdapter()
+    private var estado = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRequestpulllistBinding.inflate(layoutInflater)
@@ -26,12 +28,10 @@ class RequestPullList : AppCompatActivity() {
         val user = extra.getString("user")
         title = repo
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
-        for(i in 1..2){
-            loadLista("$user/$repo",i)
-        }
-
         initRecycleView()
+            loadLista("$user/$repo")
+
+
 
     }
 
@@ -41,23 +41,29 @@ class RequestPullList : AppCompatActivity() {
 
     }
 
-    private fun loadLista(fullname: String,page:Int) {
+    private fun loadLista(fullname: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            val call = Conexion.getRetrofit("https://api.github.com/repos/")
-                .create(APIService::class.java).getPullByRepo("$fullname/pulls?per_page=100&state=all&page=$page")//requires pagination
-            runOnUiThread {
-                if (call.isSuccessful) {
-                    val pullreq = call.body() ?: emptyList()
-                    adapter.addList(pullreq.map { Pull(it.title,it.body,it.state,it.user) })
-                    val open_count =adapter.open.size
-                    val close_count =adapter.close.size
-                    binding.opens.text= "$open_count Opens"
-                    binding.closed.text= "$close_count Closed"
+            var page =1
+            while (estado){
+                val call = Conexion.getRetrofit("https://api.github.com/repos/")
+                    .create(APIService::class.java).getPullByRepo("$fullname/pulls?per_page=100&state=all&page=$page")
+                runOnUiThread {
+                    if (call.isSuccessful) {
+                        val pullreq = call.body() ?: emptyList()
+                        adapter.addList(pullreq.map { Pull(it.title,it.body,it.state,it.user) })
+                        val open_count =adapter.open.size
+                        val close_count =adapter.close.size
+                        binding.opens.text= "$open_count Opens"
+                        binding.closed.text= "$close_count Closed"
+                        page++
+                    } else {
+                        estado=false
+                        val error_code = call.code()
+                        Toast.makeText(applicationContext,"error code: $error_code", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+            }
 
-                } else {
-                    Toast.makeText(applicationContext,"error" , Toast.LENGTH_SHORT)
-                        .show()
-                }
             }
 
         }
