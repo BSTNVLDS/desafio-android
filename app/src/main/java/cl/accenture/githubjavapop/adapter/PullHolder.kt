8d8f628bfbc1d5faf3c67ce468eedb30d23ca1c4
BSTNVLDS <bastianvaldesadasme@gmail.com>
2022-05-24 11:39:18 -0400

    add Vstate to pullList

--- app/src/main/java/cl/accenture/githubjavapop/adapter/PullHolder.kt
+++ app/src/main/java/cl/accenture/githubjavapop/adapter/PullHolder.kt
@@ -1,7 +1,9 @@
 package cl.accenture.githubjavapop.adapter
 
+import android.graphics.Color
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import cl.accenture.githubjavapop.R
 import cl.accenture.githubjavapop.databinding.ItemPullBinding
 import cl.accenture.githubjavapop.model.Pull
 import com.squareup.picasso.Picasso
@@ -13,6 +15,7 @@ class PullHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             binding.bodyPull.text= pull.body
             binding.namePull.text= pull.user!!.login
             binding.loginUserpull.text= pull.user!!.login
+            binding.state.text = pull.state
             Picasso.get().load(pull.user!!.avatar_url).into(binding.imagePull)
         }
     }
