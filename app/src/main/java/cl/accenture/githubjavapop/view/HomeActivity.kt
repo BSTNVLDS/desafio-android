06afc7f469dd6452e6ff972489e0ae988df9f9ac
BSTNVLDS <bastianvaldesadasme@gmail.com>
2022-05-26 11:51:34 -0400

    add viewmodelhome

--- app/src/main/java/cl/accenture/githubjavapop/view/HomeActivity.kt
+++ app/src/main/java/cl/accenture/githubjavapop/view/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cl.accenture.githubjavapop.R
@@ -11,76 +13,23 @@ import cl.accenture.githubjavapop.adapter.RepoAdapter
 import cl.accenture.githubjavapop.conexion.APIService
 import cl.accenture.githubjavapop.controller.Conexion
 import cl.accenture.githubjavapop.databinding.ActivityHomeBinding
+import cl.accenture.githubjavapop.viewmodel.HomeViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var viewModel: HomeViewModel
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
-    private val adapter= RepoAdapter()
-    private var _page =1
-    private val page get() = _page
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        pagination(page)
-       load()
+        viewModel = ViewModelProvider(this).get()
+       viewModel.load(binding.rc,this)
     }
-
-    private fun load() {
-        binding.rc.layoutManager = LinearLayoutManager(this)
-        binding.rc.adapter = adapter
-        adapter.setOnclickListener {
-            val positionTap = binding.rc.getChildAdapterPosition(it)
-            val selectTap = adapter.list[positionTap]
-            val intentPRL = Intent(this@HomeActivity, RequestPullList::class.java)
-           intentPRL.putExtra("repo",selectTap.name)
-            intentPRL.putExtra("user",selectTap.owner!!.login)
-            startActivity(intentPRL)
-
-        }
-        binding.rc.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                if (!recyclerView.canScrollVertically(1)){
-                    _page++
-                    pagination(page)
-            }}
-
-        })
-    }
-            private fun pagination(query:Int){
-                CoroutineScope(Dispatchers.IO).launch {
-                    val call = Conexion.getRetrofit("https://api.github.com/search/")
-                        .create(APIService::class.java).getGithubByPage("repositories?q=language:Java&sort=stars&page=$query")
-                    CoroutineScope(Dispatchers.Main).launch{
-                        if(call.isSuccessful){
-                            val tempList =call.body()?.repo ?: emptyList()
-                                for (repo in tempList){
-                                    CoroutineScope(Dispatchers.IO).launch {
-                                        val call2 = Conexion.getRetrofit("https://api.github.com/users/")
-                                            .create(APIService::class.java).getNameByUser(repo.owner?.login.toString())
-                                        CoroutineScope(Dispatchers.Main).launch{
-                                            val fullname = call2.body()?.name.toString()
-                                            repo.owner?.name = fullname
-                                            adapter.addList(repo)
-                                        }
-
-                                    }
-                            }
-
-                        }else{
-                            val ercode = call.code().toString()
-                            Toast.makeText(applicationContext,"error code: $ercode", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                }
-            }
-
-
         }
 
---
+++ app/src/main/java/cl/accenture/githubjavapop/viewmodel/HomeViewModel.kt
@@ -0,0 +1,76 @@
+package cl.accenture.githubjavapop.viewmodel
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cl.accenture.githubjavapop.adapter.RepoAdapter
+import cl.accenture.githubjavapop.conexion.APIService
+import cl.accenture.githubjavapop.controller.Conexion
+import cl.accenture.githubjavapop.view.RequestPullList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class HomeViewModel:ViewModel() {
+    private val adapter= RepoAdapter()
+    private var _page =1
+    private val page get() = _page
+     fun load(rc: RecyclerView, context : Context) {
+
+        rc.layoutManager = LinearLayoutManager(context)
+        rc.adapter = adapter
+         pagination(page,context)
+        adapter.setOnclickListener {
+            val positionTap = rc.getChildAdapterPosition(it)
+            val selectTap = adapter.list[positionTap]
+            val intentPRL = Intent(context, RequestPullList::class.java)
+            intentPRL.putExtra("repo",selectTap.name)
+            intentPRL.putExtra("user",selectTap.owner?.login)
+            context.startActivity(intentPRL)
+
+        }
+        rc.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)){
+                    _page++
+                    pagination(page,context)
+                }}
+
+        })
+    }
+
+   private fun pagination(query:Int,context:Context){
+        Log.e("","mesafeee")
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = Conexion.getRetrofit("https://api.github.com/search/")
+                .create(APIService::class.java).getGithubByPage("repositories?q=language:Java&sort=stars&page=$query")
+            CoroutineScope(Dispatchers.Main).launch{
+                if(call.isSuccessful){
+                    val tempList =call.body()?.repo ?: emptyList()
+                    for (repo in tempList){
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val call2 = Conexion.getRetrofit("https://api.github.com/users/")
+                                .create(APIService::class.java).getNameByUser(repo.owner?.login.toString())
+                            CoroutineScope(Dispatchers.Main).launch{
+                                val fullname = call2.body()?.name.toString()
+                                repo.owner?.name = fullname
+                                adapter.addList(repo)
+                            }
+
+                        }
+                    }
+
+                }else{
+                    val ercode = call.code().toString()
+                    Toast.makeText(context,"error code: $ercode", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+}
