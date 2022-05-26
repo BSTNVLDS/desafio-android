6b2971935365079f5a2690f90ddb1553cea970c4
BSTNVLDS <bastianvaldesadasme@gmail.com>
2022-05-26 12:10:08 -0400

    viewmodelrql

--- app/src/main/java/cl/accenture/githubjavapop/view/RequestPullList.kt
+++ app/src/main/java/cl/accenture/githubjavapop/view/RequestPullList.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import cl.accenture.githubjavapop.adapter.PullAdapter
 import cl.accenture.githubjavapop.conexion.APIService
@@ -12,6 +14,8 @@ import cl.accenture.githubjavapop.controller.Conexion
 import cl.accenture.githubjavapop.databinding.ActivityHomeBinding
 import cl.accenture.githubjavapop.databinding.ActivityRequestpulllistBinding
 import cl.accenture.githubjavapop.model.Pull
+import cl.accenture.githubjavapop.viewmodel.HomeViewModel
+import cl.accenture.githubjavapop.viewmodel.RequestPullListViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,8 +23,9 @@ import kotlinx.coroutines.launch
 class RequestPullList : AppCompatActivity() {
     private var _binding: ActivityRequestpulllistBinding? = null
     private val binding get() = _binding!!
-    private val adapter = PullAdapter()
-    private var estado = true
+    private var _viewModel: RequestPullListViewModel? = null
+    private val viewModel get() = _viewModel!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityRequestpulllistBinding.inflate(layoutInflater)
@@ -31,55 +36,10 @@ class RequestPullList : AppCompatActivity() {
         val user = extra?.getString("user")
         title = repo
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        initRecycleView()
-            loadLista("$user/$repo")
+        _viewModel = ViewModelProvider(this).get()
+        viewModel.initRecycleView("$user/$repo",this,binding)
 
     }
-
-    private fun initRecycleView() {
-        binding.rcr.layoutManager = LinearLayoutManager(this)
-        binding.rcr.adapter = adapter
-    }
-
-    private fun loadLista(fullname: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            var page =1
-            while (estado){
-                val call = Conexion.getRetrofit("https://api.github.com/repos/")
-                    .create(APIService::class.java).getPullByRepo("$fullname/pulls?per_page=100&state=all&page=$page")
-                runOnUiThread {
-                    if (call.isSuccessful) {
-                        val pullreq = call.body() ?: emptyList()
-                        for (repo in pullreq){
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val call2 = Conexion.getRetrofit("https://api.github.com/users/")
-                                    .create(APIService::class.java).getNameByUser(repo.user?.login.toString())
-                                CoroutineScope(Dispatchers.Main).launch{
-                                    val fullname = call2.body()?.name.toString()
-                                    repo.user?.name = fullname
-                                }
-
-                            }
-                        }
-                        adapter.addList(pullreq.map { Pull(it.title,it.body,it.state,it.user) })
-                        val open_count =adapter.open.size
-                        val close_count =adapter.close.size
-                        binding.opens.text= "$open_count Opens"
-                        binding.closed.text= "$close_count Closed"
-                        page++
-                    } else {
-                        estado=false
-                        val error_code = call.code()
-                        Toast.makeText(applicationContext,"error code: $error_code", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-            }
-
-            }
-
-        }
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
        return when (item.itemId) {
             android.R.id.home ->{
---
+++ app/src/main/java/cl/accenture/githubjavapop/viewmodel/RequestPullListViewModel.kt
@@ -0,0 +1,65 @@
+package cl.accenture.githubjavapop.viewmodel
+
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import cl.accenture.githubjavapop.adapter.PullAdapter
+import cl.accenture.githubjavapop.conexion.APIService
+import cl.accenture.githubjavapop.controller.Conexion
+import cl.accenture.githubjavapop.databinding.ActivityRequestpulllistBinding
+import cl.accenture.githubjavapop.model.Pull
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class RequestPullListViewModel : ViewModel() {
+    private val adapter = PullAdapter()
+    private var _state = true
+    private val state get() = _state!!
+
+
+     fun initRecycleView(fullname: String,context: Context,binding:ActivityRequestpulllistBinding) {
+        binding.rcr.layoutManager = LinearLayoutManager(context)
+        binding.rcr.adapter = adapter
+        loadLista(fullname,context,binding)
+    }
+   private fun loadLista(fullname: String,context: Context,binding:ActivityRequestpulllistBinding) {
+        CoroutineScope(Dispatchers.IO).launch {
+            var page =1
+            while (state){
+                val call = Conexion.getRetrofit("https://api.github.com/repos/")
+                    .create(APIService::class.java).getPullByRepo("$fullname/pulls?per_page=100&state=all&page=$page")
+                CoroutineScope(Dispatchers.Main).launch {
+                    if (call.isSuccessful) {
+                        val pullreq = call.body() ?: emptyList()
+                        for (repo in pullreq){
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val call2 = Conexion.getRetrofit("https://api.github.com/users/")
+                                    .create(APIService::class.java).getNameByUser(repo.user?.login.toString())
+                                CoroutineScope(Dispatchers.Main).launch{
+                                    val fullname = call2.body()?.name.toString()
+                                    repo.user?.name = fullname
+                                }
+
+                            }
+                        }
+                        adapter.addList(pullreq.map { Pull(it.title,it.body,it.state,it.user) })
+                        val open_count =adapter.open.size
+                        val close_count =adapter.close.size
+                        binding.opens.text= "$open_count Opens"
+                        binding.closed.text= "$close_count Closed"
+                        page++
+                    } else {
+                        _state=false
+                        val error_code = call.code()
+                        Toast.makeText(context,"error code: $error_code", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+            }
+
+        }
+    }
+}
