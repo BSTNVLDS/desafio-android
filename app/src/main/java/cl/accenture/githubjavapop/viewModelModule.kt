af1b2257e0a10af2f5843b36bce0b71abc261202
BSTNVLDS <bastianvaldesadasme@gmail.com>
2022-05-31 16:45:21 -0400

    single retrofit mth

--- app/src/main/java/cl/accenture/githubjavapop/viewModelModule.kt
+++ app/src/main/java/cl/accenture/githubjavapop/viewModelModule.kt
@@ -1,11 +1,19 @@
 package cl.accenture.githubjavapop
 
+import cl.accenture.githubjavapop.connection.GithubAPIService
 import cl.accenture.githubjavapop.viewmodel.HomeViewModel
 import cl.accenture.githubjavapop.viewmodel.RequestPullListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val viewModelModule = module {
+    single{
+        Retrofit.Builder().baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(GithubAPIService::class.java)
+    }
     viewModel {
         HomeViewModel()
     }
